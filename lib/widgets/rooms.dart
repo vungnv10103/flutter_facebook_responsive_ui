60397c5df8ca5f9ceb0707cbf41690db80030bca
Vung Nguyen <vungnguyenn1001@gmail.com>
2024-07-08 12:57:08 +0700

    ADD room, story, make responsive

---
+++ lib/widgets/rooms.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook/widgets/widgets.dart';
+
+import '../models/models.dart';
+
+class Rooms extends StatelessWidget {
+  final List<User> onlinedUser;
+  const Rooms({super.key, required this.onlinedUser});
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isDesktop = Responsive.isDesktop(context);
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: isDesktop ? 5.0 : 0.0),
+      elevation: isDesktop ? 1.0 : 0.0,
+      shape: isDesktop
+          ? RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0))
+          : null,
+      child: Container(
+        height: 60.0,
+        color: Colors.white,
+        child: ListView.builder(
+          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 4.0),
+          scrollDirection: Axis.horizontal,
+          itemCount: 1 + onlinedUser.length,
+          itemBuilder: (context, index) {
+            if (index == 0) {
+              return const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                child: _CreateRoomButton(),
+              );
+            }
+            final User user = onlinedUser[index - 1];
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: ProfileAvatar(
+                imageUrl: user.imageUrl,
+                isActive: true,
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class _CreateRoomButton extends StatelessWidget {
+  const _CreateRoomButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      onPressed: () {
+        print("Create Room");
+      },
+      child: const Row(
+        children: [
+          // ShaderMask(
+          //   shaderCallback: (rect) =>
+          //       Palette.createRoomGradient.createShader(rect),
+          //   child: const Icon(
+          //     Icons.video_call,
+          //     size: 35,
+          //     color: Colors.white,
+          //   ),
+          // ),
+          Icon(
+            Icons.video_call,
+            size: 35,
+            color: Colors.purple,
+          ),
+          SizedBox(width: 4.0),
+          Text("Create\nRoom")
+        ],
+      ),
+    );
+  }
+}
